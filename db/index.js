780135d2ac86ce5452d687f295f19e0c34e2764b
achiyahb <achiyahb@gmail.com>
2021-01-21 18:10:35 +0200

    change the connection method from client to pool for many connections

--- db/index.js
+++ db/index.js
@@ -1,23 +1,48 @@
-const { Client } = require('pg');
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-client.connect();
-function queryAction(query){
-    client.query(query, (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
+const { Pool } = require('pg');
+let pool
+if(process.env.DATABASE_URL){
+    console.log(process.env.DATABASE_URL)
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
         }
-        client.end();
     });
+} else {
+    console.log('local-server')
+    pool = new Pool({
+        "host": "localhost",
+        "port": "5432",
+        "user": "postgres",
+        "password" : "test123",
+        "database" : "postgres",
+        "max" : 20,
+        "connectionTimeoutMillis" : 2000,
+        "idleTimeoutMillis" : 30000
+    });
+}
+
+
+
+pool.connect();
+function queryAction(query){
+    try{
+        pool.query(query, (err, res) => {
+            if (err) throw err;
+            for (let row of res.rows) {
+                console.log(JSON.stringify(row));
+            }
+        });
+    } catch (e){
+        console.log(e)
+    }
+
 }
 
 module.exports = {
     queryAction
 }
+
+
+
+
